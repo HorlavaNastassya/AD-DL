58e02321d76c17a9bd908285925bedab6563fa2c
horlavanasta <nastassya.horlava@mailbox.tu-dresden.de>
2021-05-13 20:35:52 +0200

    bash script for testing muliple folders

---
+++ clinicaaddl/scripts/test_experiment_MS.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+module load anaconda/3/2020.02
+module load cuda/10.2
+module load pytorch/gpu/1.6.0
+
+# Experiment training CNN
+
+# Importnant args
+
+BAYESIAN=True
+NBR_BAYESIAN_ITER=10
+
+if [ -z "$1" ]
+  then
+    MS='1.5T-3T'
+else
+  MS=$1
+  
+  
+fi
+for NETWORK in "ResNet18" "SEResNet18" "ResNet18Expanded" "SEResNet18Expanded" "Conv5_FC3"
+do
+                # Input arguments to clinicaaddl
+    CAPS_DIR="$HOME/MasterProject/DataAndExperiments/Data/CAPS"
+    if [ $BAYESIAN = True ]; then
+        NN_FOLDER="NNs_Bayesian"
+    else
+        NN_FOLDER="NNs"
+    fi
+                
+    TSV_PATH="$HOME/MasterProject/DataAndExperiments/Experiments/Experiments-${MS}/labels/test"
+    OUTPUT_DIR="$HOME/MasterProject//DataAndExperiments/Experiments/Experiments-${MS}/${NN_FOLDER}/${NETWORK}/"
+    POSTFIX="test_${MS}"
+                # Dataset Management
+    for f in ${OUTPUT_DIR}/*; do
+        if [ -d "$f" ] &&  [[ $f =~ "subject_model" ]]; then
+                # Will not run if no directories are available
+            echo -e "$f"
+            python $HOME/MasterProject/Code/ClinicaTools/AD-DL/clinicaaddl/clinicaaddl/main.py classify $CAPS_DIR $TSV_PATH $f $POSTFIX --bayesian $BAYESIAN --nbr_bayesian_iter $NBR_BAYESIAN_ITER --selection_metrics balanced_accuracy loss last_checkpoint
+            
+            if [MS=="1.5T"]; then
+                TEST_MS="3T"
+                TEST_POSTFIX="test_${TEST_MS}"
+                TEST_TSV_PATH="$HOME/MasterProject/DataAndExperiments/Experiments/Experiments-${TEST_MS}/labels/"
+                python $HOME/MasterProject/Code/ClinicaTools/AD-DL/clinicaaddl/clinicaaddl/main.py classify $CAPS_DIR $TEST_TSV_PATH $f $TEST_POSTFIX  --bayesian $BAYESIAN --nbr_bayesian_iter $NBR_BAYESIAN_ITER --selection_metrics balanced_accuracy loss last_checkpoint --baseline False
+            fi
+            
+            if [MS=="3T"]; then
+                TEST_MS="1.5T"
+                TEST_POSTFIX="test_${TEST_MS}"
+                TEST_TSV_PATH="$HOME/MasterProject/DataAndExperiments/Experiments/Experiments-${TEST_MS}/labels/"
+                python $HOME/MasterProject/Code/ClinicaTools/AD-DL/clinicaaddl/clinicaaddl/main.py classify $CAPS_DIR $TEST_TSV_PATH $f $TEST_POSTFIX  --bayesian $BAYESIAN --nbr_bayesian_iter $NBR_BAYESIAN_ITER --selection_metrics balanced_accuracy loss last_checkpoint --baseline False
+            fi
+
+        fi
+    done                
+done
+
