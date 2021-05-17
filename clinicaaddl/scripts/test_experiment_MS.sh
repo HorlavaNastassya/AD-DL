d8954a9fbed6fa16703aae3599d5f5488bff0a15
horlavanasta <nastassya.horlava@mailbox.tu-dresden.de>
2021-05-17 15:24:27 +0200

    fixed

--- clinicaaddl/scripts/test_experiment_MS.sh
+++ clinicaaddl/scripts/test_experiment_MS.sh
@@ -25,10 +25,12 @@ if [ -z "$1" ]
   then
     MS='1.5T-3T'
 else
-  MS=$1
+    MS=$1
 fi
 
-for NETWORK in "ResNet18" "SEResNet18" "ResNet18Expanded" "SEResNet18Expanded" "Conv5_FC3"
+# for NETWORK in "ResNet18" "SEResNet18" "ResNet18Expanded" "SEResNet18Expanded" "Conv5_FC3"
+for NETWORK in "Conv5_FC3"
+
 do
                 # Input arguments to clinicaaddl
     CAPS_DIR="$HOME/MasterProject/DataAndExperiments/Data/CAPS"
